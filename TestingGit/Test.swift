59e5465bc53867f95751c0389f7d68a4b7ee9766
MCS Devices <mcsdevices@MCSs-MacBook-Air-2.local>
2017-11-16 13:33:21 -0500

    - New class added

---
+++ TestingGit/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  TestingGit
+//
+//  Created by MCS Devices on 11/16/17.
+//  Copyright © 2017 MCS Devices. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    
+}
