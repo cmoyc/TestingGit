98b61d2c0281ba173630abc7aca04b57c616d6aa
MCS Devices <mcsdevices@MCSs-MacBook-Air-2.local>
2017-11-16 13:19:53 -0500

    - Added the first button and action to the storyboard and ViewController

--- TestingGit/ViewController.swift
+++ TestingGit/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var buttonTest: UIButton!
+    
+    // MARK: Actions
+    @IBAction func didPressTestButton(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
+    
+    // MARK: properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
